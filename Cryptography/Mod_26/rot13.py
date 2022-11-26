c6370ce0feba7adc9c20350d22ff32db69784c9c
eien-ai <eien.65105@gmail.com>
2022-11-26 13:46:10 -0800

    Converting rot13.py to use 4 space indents

--- Cryptography/Mod_26/rot13.py
+++ Cryptography/Mod_26/rot13.py
@@ -1,15 +1,17 @@
 def rot13(plaintext):
-  ciphertext = []
-  for ch in plaintext:
-    if str.isalpha(ch):
-      base = 'A' if str.isupper(ch) else 'a'
-      ciphertext.append(chr((ord(ch) - ord(base) + 13) % 26 + ord(base)))
-    else:
-      ciphertext.append(ch)
-  return "".join(ciphertext)
+    ciphertext = []
+    for ch in plaintext:
+        if str.isalpha(ch):
+            base = 'A' if str.isupper(ch) else 'a'
+            ciphertext.append(chr((ord(ch) - ord(base) + 13) % 26 + ord(base)))
+        else:
+            ciphertext.append(ch)
+    return "".join(ciphertext)
+
 
 def main():
-  inp = input()
-  print(rot13(inp));
+    inp = input()
+    print(rot13(inp))
+
 
 main()
